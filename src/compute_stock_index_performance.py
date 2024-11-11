66a83b963c0ef63c259d7d09b7811b3e9c7cd99c
Murphy <duyanhv@sfu.ca>
2024-11-10 19:47:22 -0800

    fix the alternative stock performance to use close instead

--- src/compute_stock_index_performance.py
+++ src/compute_stock_index_performance.py
@@ -4,14 +4,14 @@ import pandas as pd
 
 
 def extract_quarter(df):
-    open_value = df.iloc[0]["Open"]
+    open_value = df.iloc[0]["Close"]
     close_value = df.iloc[-1]["Close"]
     peformance_percentage = ((close_value / open_value) - 1) * 100.0
     return peformance_percentage
 
 
 def extract_quarter_performance(df):
-    quarter_performance = df.groupby(by=["Quarter"]).apply(extract_quarter)
+    quarter_performance = df.groupby(by=["Quarter"])[['Quarter', 'Close', 'Company']].apply(extract_quarter)
     company_name = pd.unique(df["Company"])[0]
     performance = [np.nan, np.nan]
     if '2023Q1' in quarter_performance.index:
@@ -26,19 +26,20 @@ def extract_quarter_performance(df):
 
 def extract_stock_performance(df):
     df['Quarter'] = df["Date"].dt.to_period("Q")
-    stock_performance = df.groupby(by=["Company"]).apply(extract_quarter_performance)
+    stock_performance = df.groupby(by=["Company"])[['Quarter', 'Close', 'Company']].apply(extract_quarter_performance)
     return stock_performance
 
 
 def main():
     index_data = pd.read_csv("../data/index_data.csv", parse_dates=[0], date_format='%Y-%m-%d')
     index_performance = extract_stock_performance(index_data)
+    index_performance.to_csv("../data/index_performance.csv", index=False)
     snp_index = index_performance.loc["^GSPC"]
     stocks_data = pd.read_csv("../data/stocks_data.csv", parse_dates=[0], date_format='%Y-%m-%d')
     stocks_performance = extract_stock_performance(stocks_data)
     stocks_performance["Relative 2023Q1"] = stocks_performance['Performance 2023Q1'] - snp_index['Performance 2023Q1']
     stocks_performance["Relative 2023Q2"] = stocks_performance['Performance 2023Q2'] - snp_index['Performance 2023Q2']
-    stocks_performance.to_csv("data/stocks_performance.csv", index=False)
+    stocks_performance.to_csv("../data/stocks_performance.csv", index=False)
 
 
 if __name__ == '__main__':
