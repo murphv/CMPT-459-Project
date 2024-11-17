a84c1acc44af9968e9de22654da61ec0e257b911
Murphy <duyanhv@sfu.ca>
2024-11-16 18:43:32 -0800

    modified code to merge nlp data with stock data

--- src/organize_data_by_stocks.py
+++ src/organize_data_by_stocks.py
@@ -18,11 +18,11 @@ def extract_quarter_data(df):
     close_last_day = np.roll(df["Close"].to_numpy(), 1)[1:-1]
     close_daily = df["Close"].to_numpy()[1:-1]
     close_daily_performance = ((close_daily / close_last_day) - 1) * 100
-    stock_series = pd.Series({'Company': pd.unique(df['Company'])[0], 'Last day Open': open_value,
-                              'Begin day Close': begin_close, 'Last day Close': close_value,
-                              'Average Price': average_close, 'Average Performance': np.mean(close_daily_performance),
-                              'Volume': volume, 'Average Volume': average_volume,
-                              'Quarter High': high, 'Quarter Low': low})
+    stock_series = pd.Series({'Company': pd.unique(df['Company'])[0], 'Last Open': open_value,
+                              'Begin Close': begin_close, 'Last Close': close_value,
+                              'Average Daily Price': average_close, 'Average Daily Performance': np.mean(close_daily_performance),
+                              'Volume': volume, 'Average Daily Volume': average_volume,
+                              'High': high, 'Low': low})
     return stock_series
 
 
@@ -38,12 +38,14 @@ def extract_stock(df):
 def main():
     stock_performance = pd.read_csv("../data/stocks_performance.csv")
     stock_info = pd.read_csv("../data/stocks_info.csv")
+    nlp_data = pd.read_csv("../data/nlp_features_reduced.csv")
 
     stock_data = pd.read_csv("../data/stocks_data.csv", parse_dates=[0], date_format='%Y-%m-%d')
     stock_data_extract = extract_stock(stock_data)
     df_merge1 = pd.merge(stock_data_extract, stock_performance, on='Company')
     df_merge2 = pd.merge(df_merge1, stock_info, on='Company')
-    df_merge2.to_csv("../data/stocks_data_processed.csv", index=False)
+    df_merge3 = pd.merge(df_merge2, nlp_data, on='Company')
+    df_merge3.to_csv("../data/stocks_data_processed.csv", index=False)
 
 
 if __name__ == '__main__':
