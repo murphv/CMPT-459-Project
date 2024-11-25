30af7ef65596ec5c42a780a3eadec1fde9e351fa
pna27@sfu.ca <parham.snp@gmail.com>
2024-11-25 04:11:24 -0800

    added grid search hyper parameter tuning

--- src/classification_tuning.py
+++ src/classification_tuning.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 from sklearn.svm import SVC, SVR
 from sklearn.metrics import (
@@ -9,6 +9,7 @@ from sklearn.metrics import (
     classification_report, ConfusionMatrixDisplay,
     mean_squared_error, mean_absolute_error, r2_score
 )
+import seaborn as sns
 
 
 def evaluate_classification(y_true, y_pred, model_name):
@@ -76,30 +77,88 @@ def main():
     stocks_test = data_test['Stock'].reset_index(drop=True)
 
     # i) [Random Forest]
-    forert_model_cls = RandomForestClassifier(n_estimators=100, random_state=42)
-    forert_model_cls.fit(X_train, y_train)
-    forest_preds_cls = forert_model_cls.predict(X_test)
-    
+    forest_grid_param = {
+        'n_estimators': [100, 200, 500, 100],
+        'max_depth': [None, 10, 25 ,100],
+        'min_samples_split': [2, 5, 10, 25]
+    }
+    forert_model_cls = RandomForestClassifier(random_state=42)
+    forest_grid_search_cls = GridSearchCV(
+        estimator=forert_model_cls,
+        param_grid=forest_grid_param,
+        cv=5,
+        scoring='accuracy',
+        n_jobs=-1,
+        verbose=1
+    )
+    forest_grid_search_cls.fit(X_train, y_train)
+    best_forest = forest_grid_search_cls.best_estimator_
+    forest_preds_cls = best_forest.predict(X_test)
     evaluate_classification(y_test, forest_preds_cls, 'forest')
+
+    plt.figure(figsize=(16, 12))
+    sns.lineplot(
+        data=forest_grid_search_cls.cv_results_,
+        x='param_n_estimators',
+        y='mean_test_score',
+        hue='param_max_depth',
+        style='param_min_samples_split',
+        markers=True,
+        dashes=False
+    )
+    plt.title('Random Forest Classifier Hyperparameter Tuning')
+    plt.xlabel('Number of Trees (n_estimators)')
+    plt.ylabel('Mean Test Accuracy')
+    plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
+    plt.savefig('output/plot/rf_classifier_tuning.png', bbox_inches='tight')
+    plt.close()
     
-    disp = ConfusionMatrixDisplay.from_estimator(forert_model_cls, X_test, y_test, cmap=plt.cm.Blues)
+    disp = ConfusionMatrixDisplay.from_estimator(best_forest, X_test, y_test, cmap=plt.cm.Blues)
     disp.ax_.set_title("Random Forest Confusion Matrix")
     disp.ax_.tick_params(axis='both', which='major', labelsize=5)
     plt.savefig('output/plot/forest_confusion.png')
 
     # ii) [SVM]
-    svc_model = SVC(kernel='rbf', probability=True, random_state=42)
-    svc_model.fit(X_train, y_train)
-    svc_preds = svc_model.predict(X_test)
+    param_grid_svm = {
+        'C': [1, 5, 10, 20],
+        'gamma': ['scale', 'auto'],
+        'kernel': ['rbf', 'linear', 'sigmoid', 'poly']
+    }
+    svc = SVC(probability=True, random_state=42)
+    grid_search_svc = GridSearchCV(
+        estimator=svc,
+        param_grid=param_grid_svm,
+        cv=5,
+        scoring='accuracy',
+        n_jobs=-1,
+        verbose=1
+    )
+    grid_search_svc.fit(X_train, y_train)
+    best_svc = grid_search_svc.best_estimator_
+    svc_preds = best_svc.predict(X_test)
+    evaluate_classification(y_test, svc_preds, 'svc')
 
-    evaluate_classification(y_test, svc_preds, 'svm')
+    plt.figure(figsize=(16, 12))
+    svc_results = pd.DataFrame(grid_search_svc.cv_results_)
+    pivot_table = svc_results.pivot_table(
+        values='mean_test_score',
+        index='param_C',
+        columns='param_kernel'
+    )
+    sns.heatmap(pivot_table, annot=True, fmt=".4f")
+    plt.title('SVM Classifier Hyperparameter Tuning')
+    plt.xlabel('Kernel')
+    plt.ylabel('C')
+    plt.savefig('output/plot/svc_classifier_tuning.png', bbox_inches='tight')
+    plt.close()
+    
 
     # iii) [Baseline Classifier - Random Labels]
     np.random.seed(42)
     random_preds_cls = np.random.choice(y_train.unique(), size=len(y_test))
     evaluate_classification(y_test, random_preds_cls, 'baseline_random')
 
-    disp = ConfusionMatrixDisplay.from_estimator(svc_model, X_test, y_test, cmap=plt.cm.Blues)
+    disp = ConfusionMatrixDisplay.from_estimator(best_svc, X_test, y_test, cmap=plt.cm.Blues)
     disp.ax_.set_title("SVM Confusion Matrix")
     disp.ax_.tick_params(axis='both', which='major', labelsize=5)
     plt.savefig('output/plot/svm_confusion.png')
@@ -122,27 +181,86 @@ def main():
     stocks_test = data_test['Stock'].reset_index(drop=True)
 
     # i) [Random Forest]
-    forest_model_reg = RandomForestRegressor(n_estimators=100, random_state=42)
-    forest_model_reg.fit(X_train, y_train)
-    forest_preds_reg = forest_model_reg.predict(X_test)
-    evaluate_regression(y_test, forest_preds_reg, 'forest')
+    forest_reg = RandomForestRegressor(random_state=42)
+    forest_grid_search_reg = GridSearchCV(
+        estimator=forest_reg,
+        param_grid=forest_grid_param,
+        cv=5,
+        scoring='neg_mean_squared_error',
+        n_jobs=-1,
+        verbose=1
+    )
+    forest_grid_search_reg.fit(X_train, y_train)
+    best_forest = forest_grid_search_reg.best_estimator_
+    forest_preds = best_forest.predict(X_test)
+    evaluate_regression(y_test, forest_preds, 'forest')
+
+    rf_reg_results = pd.DataFrame(forest_grid_search_reg.cv_results_)
+    plt.figure(figsize=(16,12))
+    sns.lineplot(
+        data=rf_reg_results,
+        x='param_n_estimators',
+        y=-rf_reg_results['mean_test_score'],  # Negative MSE to MSE
+        hue='param_max_depth',
+        style='param_min_samples_split',
+        markers=True,
+        dashes=False
+    )
+    plt.title('Random Forest Regressor Hyperparameter Tuning')
+    plt.xlabel('Number of Trees (n_estimators)')
+    plt.ylabel('Mean Test MSE')
+    plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
+    plt.savefig('output/plot/rf_regressor_tuning.png', bbox_inches='tight')
+    plt.close()
 
     # ii) [SVM]
-    svr_model = SVR(kernel='rbf')
-    svr_model.fit(X_train, y_train)
-    svr_preds = svr_model.predict(X_test)
-    evaluate_regression(y_test, svr_preds, 'svm')
+    param_grid_svr = {
+        'C': [0.1, 1, 10],
+        'gamma': ['scale', 'auto'],
+        'epsilon': [0.1, 0.2, 0.5],
+        'kernel': ['rbf', 'linear', 'sigmoid', 'poly']
+    }
+    svr = SVR()
+    grid_search_svr = GridSearchCV(
+        estimator=svr,
+        param_grid=param_grid_svr,
+        cv=5,
+        scoring='neg_mean_squared_error',
+        n_jobs=-1,
+        verbose=1
+    )
+    
+    grid_search_svr.fit(X_train, y_train)
+    best_svr = grid_search_svr.best_estimator_
+    svr_preds = best_svr.predict(X_test)
+    evaluate_regression(y_test, svr_preds, 'svr')
+    
+    # Plotting Grid Search Results for SVR Regressor
+    svr_results = pd.DataFrame(grid_search_svr.cv_results_)
+    pivot_table = svr_results.pivot_table(
+        values='mean_test_score',
+        index='param_C',
+        columns='param_kernel'
+    )
+    plt.figure(figsize=(16,12))
+    sns.heatmap(-pivot_table, annot=True, fmt=".4f")  # Negative MSE to MSE
+    plt.title('SVR Regressor Hyperparameter Tuning')
+    plt.xlabel('Kernel')
+    plt.ylabel('C')
+    plt.savefig('output/plot/svr_regressor_tuning.png', bbox_inches='tight')
+    plt.close()
+    
 
     # iii) [Baseline Regressor - Predict Zero]
     np.random.seed(42)
-    random_preds_reg = np.random.uniform(-50, 50, size=len(y_test))
+    random_preds_reg = np.random.uniform(-200, 200, size=len(y_test))
     evaluate_regression(y_test, random_preds_reg, 'baseline_random')
 
     # Saving the predictions along with the stocks name
     pd.DataFrame({
     'Stock': stocks_test,
     'True Value': y_test.reset_index(drop=True),
-    'Forest Prediction': forest_preds_reg,
+    'Forest Prediction': forest_preds,
     'SVM Prediction': svr_preds
     }).sort_values(by='True Value', ascending=False).to_csv('output/regression_results.csv')
 
