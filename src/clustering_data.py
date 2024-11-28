0f081493dd7684cf37729ca3e82fe76b7fcc7978
Murphy <duyanhv@sfu.ca>
2024-11-27 16:15:29 -0800

    move scatter to eda

--- src/clustering_data.py
+++ src/clustering_data.py
@@ -8,24 +8,6 @@ from sklearn.decomposition import PCA
 from preprossessing_data import encode_category
 
 
-def clustering_by_class(df):
-    df_classes = df['Relative 2023Q2 Label']
-    unique_classes = ['Exceptionally Bad Performance', 'Bad Performance', 'Neutral', 'Good Performance']
-    df_encoded = encode_category(df.copy())
-    df_encoded = df_encoded.drop(columns=['Relative 2023Q2', 'Relative 2023Q2 Label', 'Stock'])
-    df_encoded_pca = PCA(n_components=2, random_state=459).fit_transform(df_encoded)
-
-    fig, ax = plt.subplots(figsize=(10, 10))
-    colours = ['tab:red', 'tab:orange', 'tab:green', 'tab:blue']
-    ax.set_title("Clustering result from label")
-    fig.tight_layout()
-    for (class_, colour) in zip(unique_classes, colours):
-        df_class = df_encoded_pca[df_classes == class_]
-        ax.scatter(df_class[:, 0], df_class[:, 1], s=10, c=colour, label=class_)
-    plt.legend()
-    plt.savefig('output/plot/cluster_label')
-    plt.close(fig)
-
 
 def clustering_DBSCAN(df):
     df_encoded = encode_category(df.copy())
@@ -113,7 +95,6 @@ def clustering_kmeans_plusplus(df):
 
 def main():
     df = pd.read_csv('data/stocks_data_normalized.csv')
-    clustering_by_class(df)
     clustering_DBSCAN(df)
     clustering_kmeans_plusplus(df)
 
--- src/exploratory_data_analysis.py
+++ src/exploratory_data_analysis.py
@@ -5,6 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import seaborn as sns
+from sklearn.decomposition import PCA
 
 from preprossessing_data import encode_category
 
@@ -22,14 +23,21 @@ def plot_histogram(df):
         ax.set_ylabel('count')
     fig.savefig('output/plot/overall_distribution')
     plt.close(fig)
+
+
 def plot_heatmap(df):
     df_encoded = encode_category(df.copy())
-    df_encoded = df_encoded.drop(columns=['Relative 2023Q2 Label', 'Relative 2023Q2'])
+    df_encoded = df_encoded[
+        ['Last Open', 'Begin Close', 'Last Close', 'Average Daily Price', 'Average Daily Performance',
+         'Volume', 'Average Daily Volume', 'High', 'Low', 'Performance 2023Q1', 'fullTimeEmployees',
+         'sector', 'exchange', 'recommendationKey', 'country', 'BERT_PCA_0', 'BERT_PCA_1', 'BERT_PCA_2', 'BERT_PCA_40',
+         'BERT_PCA_41']]
     fig, ax = plt.subplots(figsize=(10, 10))
     sns.heatmap(df_encoded.corr(numeric_only=True), cmap="YlGnBu")
     ax.set_title("Correlations Heatmap between features")
     fig.savefig("output/plot/heatmap")
     plt.close(fig)
+
 
 def plot_cat_histogram(df):
     fig, axd = plt.subplot_mosaic([['sector', 'sector', 'sector'],
@@ -44,10 +52,28 @@ def plot_cat_histogram(df):
         ax.set_ylabel('Count')
 
         x_tick = ax.get_xticklabels()
-        x_tick = [label.get_text().replace(' ', '\n') for label in x_tick ]
-        print(x_tick)
+        x_tick = [label.get_text().replace(' ', '\n') for label in x_tick]
         ax.set_xticklabels(x_tick)
     plt.savefig('output/plot/cat_distribution')
+    plt.close(fig)
+
+
+def clustering_by_class(df):
+    df_classes = df['Relative 2023Q2 Label']
+    unique_classes = ['Exceptionally Bad Performance', 'Bad Performance', 'Neutral', 'Good Performance']
+    df_encoded = encode_category(df.copy())
+    df_encoded = df_encoded.drop(columns=['Relative 2023Q2', 'Relative 2023Q2 Label', 'Stock'])
+    df_encoded_pca = PCA(n_components=2, random_state=459).fit_transform(df_encoded)
+
+    fig, ax = plt.subplots(figsize=(8, 8))
+    colours = ['tab:red', 'tab:orange', 'tab:green', 'tab:blue']
+    ax.set_title("Scatter plot of data by label")
+    fig.tight_layout()
+    for (class_, colour) in zip(unique_classes, colours):
+        df_class = df_encoded_pca[df_classes == class_]
+        ax.scatter(df_class[:, 0], df_class[:, 1], s=15, c=colour, label=class_)
+    plt.legend()
+    plt.savefig('output/plot/scatter_label')
     plt.close(fig)
 
 
@@ -55,8 +81,10 @@ def main():
     df_before_normalized = pd.read_csv('data/stocks_data_processed_imputed.csv')
     df_post_normalize = pd.read_csv('data/stocks_data_normalized_v1.csv')
 
+    plot_histogram(df_post_normalize)
     plot_cat_histogram(df_post_normalize)
-    plot_heatmap(df_before_normalized)
+    clustering_by_class(df_post_normalize)
+    plot_heatmap(df_post_normalize)
 
     df_post_normalize = df_post_normalize.drop(
         columns=['Volume'])  # we drop Volume since it is somewhat redundant by other features and due to sparseness
